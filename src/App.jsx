febc241f66d2ff9a37e412aa43e48837365d5367
Anas Asimi <anas2020cr7@gmail.com>
2023-06-08 15:35:32 +0100

    add react router create pages folder to organize routes fix eslintrc to use ES6 modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
-function App() {
-  return (
-    <>
-      <h1>hello</h1>
-    </>
-  )
-}
+import Layout from "./layouts/Layout";
+import { Routes, Route } from "react-router-dom";
 
-export default App
+// import pages
+import Home from "./pages/Home";
+import Storage from "./pages/Storage";
+import Pomps from "./pages/Pomps";
+import Coordinates from "./pages/Coordinates";
+import About from "./pages/About";
+import Notfound from "./pages/Notfound";
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="/storage" element={<Storage />} />
+        <Route path="/pomps" element={<Pomps />} />
+        <Route path="/coordinates" element={<Coordinates />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<Notfound />} />
+      </Route>
+    </Routes>
+  );
+}
