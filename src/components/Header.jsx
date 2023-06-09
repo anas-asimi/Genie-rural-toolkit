ac622fdde8ed23510624fcb87d07b1f831b46f6f
Anas Asimi <anas2020cr7@gmail.com>
2023-06-09 11:57:03 +0100

    improve Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import {
 	Box,
+	Center,
 	Collapse,
 	Flex,
 	HStack,
@@ -19,76 +20,79 @@ export default function Header() {
 
 	return (
 		<>
-			<Flex as="header" w="100%" p='16px' boxShadow="0px 0px 8px 0px rgba(0,0,0,0.1)">
-				<Heading>Genie Rural Toolkit</Heading>
-				<Spacer />
-				<Show below="lg">
-					<Box onClick={onToggle}>
-						{isOpen ? (
-							<IconButton
-								aria-label="close menu"
-								border="none"
-								icon={<MdOutlineClose size="40px" />}
-							/>
-						) : (
-							<IconButton
-								aria-label="open menu"
-								border="none"
-								icon={<MdMenu size="40px" />}
-							/>
-						)}
-					</Box>
-				</Show>
-				<Show above="lg">
-					<HStack as="nav" gap="24px" fontSize="lg" textTransform="capitalize">
-						<Link color="black" as={ReactLink} to="/">
+			<Center w='100%' boxShadow="0px 0px 8px 0px rgba(0,0,0,0.1)">
+				<Flex as="header" w="100%" p='16px' maxW='1200px'>
+					<Heading>Genie Rural Toolkit</Heading>
+					<Spacer />
+					<Show below="lg">
+						<Box onClick={onToggle}>
+							{isOpen ? (
+								<IconButton
+									aria-label="close menu"
+									border="none"
+									icon={<MdOutlineClose size="40px" />}
+								/>
+							) : (
+								<IconButton
+									aria-label="open menu"
+									border="none"
+									icon={<MdMenu size="40px" />}
+								/>
+							)}
+						</Box>
+					</Show>
+					<Show above="lg">
+						<HStack as="nav" gap="24px" fontSize="lg" textTransform="capitalize">
+							<Link color="black" as={ReactLink} to="/">
+								home
+							</Link>
+							<Link color="black" as={ReactLink} to="/storage">
+								storage
+							</Link>
+							<Link color="black" as={ReactLink} to="/pomps">
+								pomps
+							</Link>
+							<Link color="black" as={ReactLink} to="/coordinates">
+								coordinates
+							</Link>
+							<Link color="black" as={ReactLink} to="/about">
+								about
+							</Link>
+						</HStack>
+					</Show>
+				</Flex>
+			</Center>
+			<Show below="lg">
+				<Collapse in={isOpen}>
+					<VStack
+						as="nav"
+						w='100vw'
+						gap="8px"
+						fontSize="lg"
+						textTransform="capitalize"
+						alignItems="flex-end"
+						p='16px 32px'
+						boxShadow="0px 0px 8px 0px rgba(0,0,0,0.1)"
+						mb='4px'
+					>
+						<Link color="black" onClick={onToggle} as={ReactLink} to="/">
 							home
 						</Link>
-						<Link color="black" as={ReactLink} to="/storage">
+						<Link color="black" onClick={onToggle} as={ReactLink} to="/storage">
 							storage
 						</Link>
-						<Link color="black" as={ReactLink} to="/pomps">
+						<Link color="black" onClick={onToggle} as={ReactLink} to="/pomps">
 							pomps
 						</Link>
-						<Link color="black" as={ReactLink} to="/coordinates">
+						<Link color="black" onClick={onToggle} as={ReactLink} to="/coordinates">
 							coordinates
 						</Link>
-						<Link color="black" as={ReactLink} to="/about">
+						<Link color="black" onClick={onToggle} as={ReactLink} to="/about">
 							about
 						</Link>
-					</HStack>
-				</Show>
-
-			</Flex>
-			<Collapse in={isOpen}>
-				<VStack
-					as="nav"
-					w='100vw'
-					gap="8px"
-					fontSize="lg"
-					textTransform="capitalize"
-					alignItems="flex-end"
-					p='16px 32px'
-					boxShadow="0px 0px 8px 0px rgba(0,0,0,0.1)"
-					mb='4px'
-				>
-					<Link color="black" onClick={onToggle} as={ReactLink} to="/">
-						home
-					</Link>
-					<Link color="black" onClick={onToggle} as={ReactLink} to="/storage">
-						storage
-					</Link>
-					<Link color="black" onClick={onToggle} as={ReactLink} to="/pomps">
-						pomps
-					</Link>
-					<Link color="black" onClick={onToggle} as={ReactLink} to="/coordinates">
-						coordinates
-					</Link>
-					<Link color="black" onClick={onToggle} as={ReactLink} to="/about">
-						about
-					</Link>
-				</VStack>
-			</Collapse>
+					</VStack>
+				</Collapse>
+			</Show>
 		</>
 	);
 }
