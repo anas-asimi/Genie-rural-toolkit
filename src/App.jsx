dc7f0445a4e817f02d963df0b81c81362c737046
Anas Asimi <anas2020cr7@gmail.com>
2023-06-10 22:13:39 +0100

    use lazy load in routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,6 @@ import { Routes, Route } from "react-router-dom";
 
 // import pages
 import Home from "./pages/Home";
-import Storage from "./pages/Storage";
-import Pomps from "./pages/Pomps";
-import Coordinates from "./pages/Coordinates";
-import About from "./pages/About";
 import Notfound from "./pages/Notfound";
 
 export default function App() {
@@ -14,10 +10,10 @@ export default function App() {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path="/storage" element={<Storage />} />
-        <Route path="/pomps" element={<Pomps />} />
-        <Route path="/coordinates" element={<Coordinates />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/storage" lazy={() => import("./pages/Storage")} />
+        <Route path="/pomps" lazy={() => import("./pages/Pomps")} />
+        <Route path="/coordinates" lazy={() => import("./pages/Coordinates")} />
+        <Route path="/about" lazy={() => import("./pages/About")} />
         <Route path="*" element={<Notfound />} />
       </Route>
     </Routes>
